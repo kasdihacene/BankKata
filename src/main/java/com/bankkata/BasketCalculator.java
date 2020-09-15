bdce44c3c4097ec38898c2b241672ea78b1e8359
Hacene <hacene.upmc@gmail.com>
2020-09-15 12:27:55 +0200

    Insert in the list articles and calculate basket

--- src/main/java/com/bankkata/BasketCalculator.java
+++ src/main/java/com/bankkata/BasketCalculator.java
@@ -3,15 +3,38 @@ package com.bankkata;
 import com.bankkata.domain.Article;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BasketCalculator {
 
-    public static BigDecimal calculate(List<Article> articles) {
+    private List<Article> articles;
+
+    private BasketCalculator() {
+        this.articles = new ArrayList<>();
+    }
+
+    public static BasketCalculator instance() {
+        return new BasketCalculator();
+    }
+
+    private BigDecimal compute(List<Article> articles) {
+        return articles
+                .stream()
+                .map(Article::calculate)
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    public BigDecimal calculate(List<Article> articles) {
         return compute(articles);
     }
 
-    private static BigDecimal compute(List<Article> articles) {
-        return articles.stream().map(Article::calculate).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+    public BigDecimal calculate() {
+        return compute(this.articles);
+    }
+
+    public void add(Article snickers) {
+        this.articles.add(snickers);
     }
 }
--- src/test/java/com/bankkata/BasketTest.java
+++ src/test/java/com/bankkata/BasketTest.java
@@ -13,6 +13,13 @@ import java.util.List;
 
 public class BasketTest {
 
+    private BasketTest() {
+    }
+
+    public static BasketTest factory() {
+        return new BasketTest();
+    }
+
     @Test
     public void shouldCalculateTotalBasketWhenHavingOneArticle() {
         // GIVEN
@@ -20,7 +27,7 @@ public class BasketTest {
         // WHEN
         Price priceValue = new Price.Builder().withPrice("99.99").build();
         final Article snickers = new Article("SNICKERS", priceValue);
-        BigDecimal total = BasketCalculator.calculate(Arrays.asList(snickers));
+        BigDecimal total = BasketCalculator.instance().calculate(Collections.singletonList(snickers));
         // THEN
         Assertions.assertThat(expected).isEqualTo(total);
     }
@@ -33,7 +40,7 @@ public class BasketTest {
         // WHEN
         Price priceValue = new Price.Builder().withPrice("20.25").build();
         Article snickers = new Article("SNICKERS", priceValue, quantity);
-        BigDecimal total = BasketCalculator.calculate(Arrays.asList(snickers));
+        BigDecimal total = BasketCalculator.instance().calculate(Collections.singletonList(snickers));
         // THEN
         Assertions.assertThat(expected).isEqualTo(total);
     }
@@ -48,7 +55,7 @@ public class BasketTest {
         Article snickers = new Article("SNICKERS", priceValue, quantity);
         Article shoes = new Article("SHOES", priceValue, quantity);
         List<Article> articles = Arrays.asList(snickers, shoes);
-        BigDecimal total = BasketCalculator.calculate(articles);
+        BigDecimal total = BasketCalculator.instance().calculate(articles);
         // THEN
         Assertions.assertThat(expected).isEqualTo(total);
     }
@@ -57,11 +64,36 @@ public class BasketTest {
     public void shouldCalculateTotalWhenHavingOnlyOneArticleInTheList() {
         // GIVEN
         BigDecimal expected = new BigDecimal("99.99");
-        // WHEN
         Price priceValue = new Price.Builder().withPrice("99.99").build();
         Article snickers = new Article("SNICKERS", priceValue);
-        BigDecimal total = BasketCalculator.calculate(Collections.singletonList(snickers));
+        // WHEN
+        BigDecimal total = BasketCalculator.instance().calculate(Collections.singletonList(snickers));
         // THEN
         Assertions.assertThat(expected).isEqualTo(total);
     }
+
+    @Test
+    public void shouldReturnZeroWhenNoArticlePresentInTheList(){
+        // GIVEN
+        BigDecimal expected = new BigDecimal("0");
+        // WHEN
+        BigDecimal total = BasketCalculator.instance().calculate();
+        // THEN
+        Assertions.assertThat(expected).isEqualTo(total);
+    }
+
+    @Test
+    public void shouldCalculateAddedArticles(){
+        // GIVEN
+        BigDecimal expected = new BigDecimal("99.99");
+        Price priceValue = new Price.Builder().withPrice("99.99").build();
+        Article snickers = new Article("SNICKERS", priceValue);
+        BasketCalculator basketCalculator = BasketCalculator.instance();
+        // WHEN
+        basketCalculator.add(snickers);
+        BigDecimal total = basketCalculator.calculate();
+        // THEN
+        Assertions.assertThat(expected).isEqualTo(total);
+    }
+
 }
